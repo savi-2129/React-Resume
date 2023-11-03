274159710ba5fdd5d8fc67efd8e74714acb5e370
savi-2129 <savi.21cs089@sode-edu.in>
2023-11-03 10:49:07 +0530

    first commit

---
+++ component/Resume/Resume.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Resume() {
+  return (
+    <div><div class="heading">Resume</div>
+  <div>  <h3>Name:Savi H.S</h3>
+     <h3>Email id:savihs29@gmail.com</h3>
+     <h3>Phone number:7348868489 </h3></div>
+     
+     <br></br>
+     <br></br>
+     
+     <div class ="main">
+     <div class="H2"><h2>About Myself:</h2><div className='v1'>I'm a Third years computer science student
+    passionate in learing new things in the field of enginerring</div></div>
+
+    <div class="H3"><h2>Education:</h2><div className='v2'>school: jawahar navodaya vidhalaya chara ,hebri[2014-2019]
+      college: mother teresa pu college shankaranarayana[2019-2021]</div></div>
+      
+      <div class="H4"><h2> Technical Skills:</h2><div className='v3'></div>
+       <p> java</p>
+      <p> phython</p>
+      <p> c/c++</p>
+      <p>html</p></div>
+
+      <div class="H3"><h2>Achivment:</h2><div className='v2'>participated in the "ethical haking and cyber security " technical project ideation(competition cum exhibition)</div></div>
+       </div>
+   
+    
+     </div>
+     
+  )
+}
+
+export default Resume
